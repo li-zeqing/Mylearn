bf47279bd2b8f3c96f83ee1e23cae5dd77472980
lizeqing <lizeqing77@163.com>
2022-03-21 16:31:20 +0800

    update

--- Mylearn_java/src/com/mylove/bank/Account.java
+++ Mylearn_java/src/com/mylove/bank/Account.java
@@ -1,31 +1,33 @@
 package com.mylove.bank;
 
 public class Account {
-	
+
 	private double balance;
-	
-	public  Account(double init_balance) {
+
+	public Account(double init_balance) {
 		this.balance = init_balance;
 	}
-	
+
 	public double getBalance() {
+		
 		return balance;
-	}
 	
-	//存款
+	}
+
+	// 存款
 	public void deposit(double amt) {
-		if(amt > 0) {
+		if (amt > 0) {
 			balance += amt;
 			System.out.println("存钱成功");
 		}
 	}
-	
-	//取款
+
+	// 取款
 	public void withdraw(double amt) {
-		if(balance >= amt) {
+		if (balance >= amt) {
 			balance -= amt;
 			System.out.println("取钱成功");
-		}else {
+		} else {
 			System.out.println("余额不足");
 		}
 	}
