c492a425075ff1c7c07688b556444504bb16b8ba
lizeqing <lizeqing77@163.com>
2022-03-28 23:39:29 +0800

    update

---
+++ Mylearn_java/src/com/mylove/exception/EcmDef.java
@@ -0,0 +1,17 @@
+package com.mylove.exception;
+/**
+ * 
+ * @Description
+ * @author Zeqing Li Email:lizeqing77@163.com
+ * @version
+ * @date 2022年3月28日下午11:34:13
+ * 编写应用程序EcmDef.java,接收命令行的两发参数，要求不能输入负数，计算两数相除。
+ * 对数据类型不一致(NumberFormatException)、缺少命令行参数(ArrayIndexOutOfBoundsException)、
+ * 除0(ArithmeticException)及输入负数(EcDef 自定义的异常)进行异常处理。
+ * 提示：
+ * 	1.在主类(EcmDef)
+ * 
+ */
+public class EcmDef {
+
+}
